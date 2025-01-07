7ddd7b495ec8823e4c19e68a9d7868051ad539bf
Blue-Silvers <enzo.lahana@artfx.fr>
2025-01-07 17:35:34 +0100

    [WIP] box collider 2D header

--- AbsoluteFate/BoxCollider2D.h
+++ AbsoluteFate/BoxCollider2D.h
@@ -1,5 +1,18 @@
 #pragma once
-class BoxCollider2D
+#include "Components.h"
+#include "Transform2D.h"
+class Actor;
+
+class BoxCollider2D : public Components
 {
+private:
+	Transform2D mColliderBox;
+	Actor* mParentActor;
+public :
+	BoxCollider2D();
+	virtual void OnStart() 
+	{ 
+		mParentActor = mOwner;
+	};
 };
 
