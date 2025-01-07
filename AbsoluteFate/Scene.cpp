29c73aa4ca56fdc0cb312399b6d8c589b7c7749d
Blue-Silvers <enzo.lahana@artfx.fr>
2025-01-07 16:30:16 +0100

    [Fix] actor and scene header

--- AbsoluteFate/Scene.cpp
+++ AbsoluteFate/Scene.cpp
@@ -1,2 +1,47 @@
 #include "Scene.h"
+#include "Actor.h"
 
+//Reset scene
+Scene::~Scene()
+{
+	for (Actor* actor : mActorsList)
+	{
+		delete actor;
+	}
+}
+
+void Scene::Start()
+{
+	for (Actor* actor : mActorsList)
+	{
+		actor->Start();
+	}
+}
+
+void Scene::Update()
+{
+	for (Actor* actor : mActorsList)
+	{
+		actor->Update();
+	}
+}
+
+void Scene::AddActor(Actor* newActor)
+{
+	//Add actor in the scene
+	mActorsList.push_back(newActor);
+	newActor->AttachScene(this);
+	newActor->Start();
+}
+
+void Scene::RemoveActor(Actor* deadActor)
+{
+	for (int i = 0; i < mActorsList.size(); i++)
+	{
+		if (mActorsList[i] == deadActor)
+		{
+			mActorsList.erase(mActorsList.begin() + i);
+		}
+	}
+	deadActor->Destroy();
+}
--- AbsoluteFate/Scene.h
+++ AbsoluteFate/Scene.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "Renderer.h"
-#include "Actor.h"
 #include <iostream>
+#include <vector>
+class Actor;
+
 class Scene
 {
 private:
 	std::string mTitle = "Fate Engine";
-	std::vector<Actor*> mActorsList;
+	std::vector<Actor*> mActorsList{};
 
 protected:
 	Renderer* mRenderer;
@@ -15,15 +17,20 @@ public:
 
 	//Base of scene
 	Scene(std::string pTitle = "Scene") :mTitle(pTitle) {};
+	virtual ~Scene();
+
 	virtual void SetRenderer(Renderer* pRenderer)
 	{
 		mRenderer = pRenderer;
 	};
-	virtual void Start() {};
-	virtual void Update() {};
+	virtual void Start();
+	virtual void Update();
 	virtual void Render() {};
 	virtual void OnInput(SDL_Event) {};
 	virtual void  Close() {};
 
+	//Actors
+	void AddActor(Actor* newActor);
+	void RemoveActor(Actor* deadActor);
 };
 
